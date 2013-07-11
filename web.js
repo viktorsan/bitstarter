e0ed2e5456d13f770f6d4ab28ebd83b9a3c10078
Ubuntu <ubuntu@ip-172-31-29-132.us-west-2.compute.internal>
2013-07-11 10:49:18 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -5,8 +5,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
-  response.send(fs.readFileSync('index.html').toString);
+  response.send(fs.readFileSync('index.html',String));
 });
 
 var port = process.env.PORT || 5000;
